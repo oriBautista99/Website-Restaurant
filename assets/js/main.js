58972a661de0b6e519a5e72712b2d8a64084bf98
Oriana Bautista <oribautista99@gmail.com>
2024-09-12 10:35:57 -0400

    Add readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ function scrollActive() {
         const sectionHeight = current.offsetHeigth;
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id');
-        if(scrollY > sectionTop && scrollY <= sectionTop+secctionHeight){
+        if(scrollY > sectionTop && scrollY <= sectionTop+sectionHeight){
             document.querySelector('.nav-menu a[href*='+sectionId+']').classList.add('active-link');
         }else{
             document.querySelector('.nav-menu a[href*='+sectionId+']').classList.remove('active-link');
